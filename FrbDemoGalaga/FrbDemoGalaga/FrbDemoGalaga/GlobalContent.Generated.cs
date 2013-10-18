e49a16dab5b8551c5e00ca9a675da66850f124db
Scott Dancer <dancer.scott@gmail.com>
2013-10-17 21:56:58 -0500

    Added Nes Font.

--- FrbDemoGalaga/FrbDemoGalaga/FrbDemoGalaga/GlobalContent.Generated.cs
+++ FrbDemoGalaga/FrbDemoGalaga/FrbDemoGalaga/GlobalContent.Generated.cs
@@ -15,13 +15,24 @@ namespace FrbDemoGalaga
 	public static partial class GlobalContent
 	{
 		
+		public static Microsoft.Xna.Framework.Graphics.Texture2D GalagaSpriteSheet { get; set; }
 		[System.Obsolete("Use GetFile instead")]
 		public static object GetStaticMember (string memberName)
 		{
+			switch(memberName)
+			{
+				case  "GalagaSpriteSheet":
+					return GalagaSpriteSheet;
+			}
 			return null;
 		}
 		public static object GetFile (string memberName)
 		{
+			switch(memberName)
+			{
+				case  "GalagaSpriteSheet":
+					return GalagaSpriteSheet;
+			}
 			return null;
 		}
 		public static bool IsInitialized { get; private set; }
@@ -30,6 +41,7 @@ namespace FrbDemoGalaga
 		public static void Initialize ()
 		{
 			
+			GalagaSpriteSheet = FlatRedBallServices.Load<Microsoft.Xna.Framework.Graphics.Texture2D>(@"content/globalcontent/galagaspritesheet.png", ContentManagerName);
 						IsInitialized = true;
 		}
 		public static void Reload (object whatToReload)
